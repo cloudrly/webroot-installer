6ceed92f582675ea261d93ae5bc4a4878664c137
Olivier ORABONA <oorabona@users.noreply.github.com>
2022-11-24 14:47:57 +0100

    implement required methods

--- src/WebRootInstallerPlugin.php
+++ src/WebRootInstallerPlugin.php
@@ -13,4 +13,14 @@ class WebRootInstallerPlugin implements PluginInterface
         $installer = new WebRootInstaller($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        // Not needed
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        // Not needed
+    }
 }
